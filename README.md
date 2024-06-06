74d6008c8c2e932ac1eb840dd9ab6be828433b70
Hiram Fernandes <hiram.sd@gmail.com>
2024-06-06 18:33:45 -0300

    Created src folder and updated README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A simple go app that retrieves a single record from an existing Mongo Database
 
 Usage:
-Create a file named .env at the root level and add the following line
+Create a file named .env to the src folder and add the following line
 MONGODB_URI:"your-mongo-db-conn-string"
 
 You should be good to go
---
+++ src/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable" +
+			"See " +
+			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("control").Collection("vendors")
+	name := "Bourbon Assis Brasil"
+
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"name", name}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the vendor name %s\n", name)
+		return
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+}
